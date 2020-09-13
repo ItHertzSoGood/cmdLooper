3c48c1c42448c902ed31ea41ce2f83208bafc306
ItHertzSoGood <60296901+ItHertzSoGood@users.noreply.github.com>
2020-09-13 14:46:57 -0700

    various changes

--- cmdLooperPrefs/CMDRootListController.h
+++ cmdLooperPrefs/CMDRootListController.h
@@ -2,5 +2,7 @@
 #import <Preferences/PSSpecifier.h>
 
 @interface CMDRootListController : PSListController
-
+- (NSMutableArray *)specifiers;
+- (id)readPreferenceValue:(PSSpecifier *)specifier;
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier;
 @end
